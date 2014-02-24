1700e3af9391bdbe9670e63f31594f83627ee20e
Marcus Olsson <marcus.h.olsson@gmail.com>
2014-02-24 20:44:21 +0100

    added array for liniar brigtness

--- LED_bracelet_Acc.ino
+++ LED_bracelet_Acc.ino
@@ -7,7 +7,24 @@
 #define PIXEL_OUTPUT 1           
 #define ANALOG_INPUT 2           
 #define NUM_PIXELS 19            
-byte brightness = 30;            
+byte brightness = 180; 
+
+PROGMEM byte exp_map[256]={
+  0,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,
+  1,1,1,1,1,1,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,2,3,
+  3,3,3,3,3,3,3,3,3,3,3,3,4,4,4,4,4,4,4,4,4,4,4,5,5,5,
+  5,5,5,5,5,6,6,6,6,6,6,6,7,7,7,7,7,7,8,8,8,8,8,8,9,9,
+  9,9,10,10,10,10,10,11,11,11,11,12,12,12,13,13,13,13,
+  14,14,14,15,15,15,16,16,16,17,17,18,18,18,19,19,20,
+  20,20,21,21,22,22,23,23,24,24,25,26,26,27,27,28,29,
+  29,30,31,31,32,33,33,34,35,36,36,37,38,39,40,41,42,
+  42,43,44,45,46,47,48,50,51,52,53,54,55,57,58,59,60,
+  62,63,64,66,67,69,70,72,74,75,77,79,80,82,84,86,88,
+  90,91,94,96,98,100,102,104,107,109,111,114,116,119,
+  122,124,127,130,133,136,139,142,145,148,151,155,158,
+  161,165,169,172,176,180,184,188,192,196,201,205,210,
+  214,219,224,229,234,239,244,250,255
+};
 
 int32_t storedVector = 0,
          newVector = 0;
@@ -57,17 +74,19 @@ void loop()
   newVector += lsm.accelData.z*lsm.accelData.z;
   newVector = sqrt(newVector);
   curr_color_granularity = 1;
-  if(brightness > 30 && (millis() - brightness_decay_timer) > curr_brightness_decay_interval){
+  
+  if(brightness > 180 && (millis() - brightness_decay_timer) > curr_brightness_decay_interval){
     brightness--;
     brightness_decay_timer = millis();
   }
-  pixels.setBrightness(brightness);
+  
+  pixels.setBrightness(exp_map[brightness]);
   
   if (abs(newVector - storedVector) > MOVE_THRESHOLD) {
     curr_color_granularity = 20;
     nextColor();
     last_shacke = millis(); 
-    if(brightness < 150) brightness += 10;
+    if(brightness < 255) brightness += 10;
     nr_shackes++;
   };
   
